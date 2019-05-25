be2d019b45a9d6f4559bd48ac091bd2572bc59f4
Clark Tyra <clark.tyra@gmail.com>
2019-05-25 16:14:35 -0700

    1

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,46 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+//my first solution
+function reverse(str) {
+    let arr = [];
+    for (let char of str) {
+        arr.unshift(char)
+    }; 
+    return arr.toString().replace(/,/g, "");
+}
+
 
 module.exports = reverse;
+
+//solution 1
+// convert string to array
+// uses array.reverse()
+// function reverse(str) {
+//     // const arr = str.split('');
+//     // arr.reverse();
+//     // return arr.join('')
+//     return str.split('').reverse().join('')
+// }
+
+
+// module.exports = reverse;
+
+// function reverse(str) {
+//     let reversed = '';
+
+//     for (let character of str){
+//         reversed = character + reversed;
+//     }
+//     return reversed;
+// }
+
+
+// module.exports = reverse;
+
+// function reverse(str) {
+//     return str.split('').reduce((rev, char) => char + rev, '');
+// }
+
+
+// module.exports = reverse;
