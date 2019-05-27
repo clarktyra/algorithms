a73432a2ce4a9c895b87b1bcdadefa372b5e1e23
Clark Tyra <clark.tyra@gmail.com>
2019-05-27 07:35:58 -0700

    completed sections 9-11, anagrams, sentance capitalization, & Steps

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,58 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// function anagrams(stringA, stringB) {
+//     //taking the non characters out of string
+//     //convert str to lowercase
+//     let myStringA = stringA.replace(/[^\w]/g, '').toLowerCase();
+//     let myStringB = stringB.replace(/[^\w]/g, '').toLowerCase();
 
+//     //see if the strings have the same number of character
+    
+//     if (myStringA.length !== myStringB.length) {
+//         return false;
+//     }
+
+//     // convert strings to object trees
+//     const obj1 = {};
+//     for (let char of myStringA) {
+//         if (obj1[char]) {
+//             obj1[char]++;
+//         } else {
+//             obj1[char] = 1;
+//         }
+//     }
+
+//     const obj2 = {};
+//     for (let char of myStringB) {
+//         if (obj2[char]) {
+//             obj2[char]++;
+//         } else {
+//             obj2[char] = 1;
+//         }
+//     }
+
+//     for (let char in obj1) {
+//         if (obj1[char] ==! obj2[char]){
+//             return false;
+//         }
+//     }
+
+//     return true;
+
+
+
+// }
+// module.exports = anagrams;
+
+
+function anagrams(stringA, stringB) {
+    //compare the tho ordered strings
+    return sortString(stringA) === sortString(stringB)
+}
+function sortString(str) {
+    //remove noncharacters from string, make characters lowercase
+    //use split to make it an array, use sort to order it, use join to convert back to string
+    return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('')
+}
 module.exports = anagrams;
--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,36 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+// function capitalize(str) {
+//     const finalWords = [];
+//     let newArray = str.split(' ')
+//     console.log(newArray)
+    
+//     for (let word of newArray){
+//         finalWords.push(word[0].toUpperCase() + word.slice(1)) 
+//     }
+
+//     return finalWords.join(' ')
+    
+// }
+
+// module.exports = capitalize;
+
+
+
+function capitalize(str) {
+    let word = str[0].toUpperCase();
+
+    for (let i = 1; i < str.length ; i++){
+        if (str[i-1] == ' '){
+            word += str[i].toUpperCase();
+        } else {
+            word += str[i];
+        }
+    }
+
+    return word;
+    
+}
 
 module.exports = capitalize;
